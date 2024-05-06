b2cb4d67c8d66162d5f6e4e36b5589fed0771f6e
Pecas321 <danielc.salinasr@utadeo.edu.co>
2024-05-05 23:50:23 -0500

    catorce commit

--- app/src/main/java/com/example/tadeonow/Biblioteca.kt
+++ app/src/main/java/com/example/tadeonow/Biblioteca.kt
@@ -8,26 +8,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Deportes : AppCompatActivity() {
+class Biblioteca : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_deportes)
-
-        val btnUsuario = findViewById<ImageButton>(R.id.bt_usuario_deportes)
+        setContentView(R.layout.activity_biblioteca)
+        val btnUsuario = findViewById<ImageButton>(R.id.bt_usuario_biblioteca)
         btnUsuario.setOnClickListener {
             val intent = Intent(this, usuario::class.java)
             startActivity(intent)
         }
-        val btnPlanes = findViewById<ImageButton>(R.id.casa_deportes)
+        val btnPlanes = findViewById<ImageButton>(R.id.casa_biblioteca)
         btnPlanes.setOnClickListener {
             val intent = Intent(this, planes::class.java)
             startActivity(intent)
         }
-        val btnAgregarPlan = findViewById<ImageButton>(R.id.crea_deportes)
+        val btnAgregarPlan = findViewById<ImageButton>(R.id.crea_biblioteca)
         btnAgregarPlan.setOnClickListener {
             val intent = Intent(this, agregar_plan::class.java)
             startActivity(intent)
         }
+
     }
 }
--- app/src/main/java/com/example/tadeonow/Informacion.kt
+++ app/src/main/java/com/example/tadeonow/Informacion.kt
@@ -2,6 +2,7 @@ package com.example.tadeonow
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -26,6 +27,22 @@ class Informacion : AppCompatActivity() {
         val btnAgregarPlan = findViewById<ImageButton>(R.id.crea_info)
         btnAgregarPlan.setOnClickListener {
             val intent = Intent(this, agregar_plan::class.java)
+            startActivity(intent)
+        }
+
+        val btnDeportes = findViewById<Button>(R.id.Bt_deportes)
+        btnDeportes.setOnClickListener {
+            val intent = Intent(this, Deportes::class.java)
+            startActivity(intent)
+        }
+        val btnBiblioteca = findViewById<Button>(R.id.Bt_biblioteca)
+        btnBiblioteca.setOnClickListener {
+            val intent = Intent(this, Biblioteca::class.java)
+            startActivity(intent)
+        }
+        val btnSalones = findViewById<Button>(R.id.Bt_salones)
+        btnSalones.setOnClickListener {
+            val intent = Intent(this, Salones::class.java)
             startActivity(intent)
         }
 
