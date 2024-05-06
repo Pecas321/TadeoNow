a228844660eb9c9b6ac4a1f4248b1c4a7ebb912e
Pecas321 <danielc.salinasr@utadeo.edu.co>
2024-05-06 02:19:09 -0500

    diesisiete commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
+    implementation ("com.google.firebase:firebase-firestore-ktx:23.0.1")
 }
