9f0a4c9540f2c5655fddcf484ae3a58746d2a1ef
Pecas321 <danielc.salinasr@utadeo.edu.co>
2024-05-05 23:17:19 -0500

    doce commit

--- app/src/main/java/com/example/tadeonow/agregar_plan.kt
+++ app/src/main/java/com/example/tadeonow/agregar_plan.kt
@@ -3,6 +3,7 @@ package com.example.tadeonow
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,7 +14,7 @@ class agregar_plan : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_plan)
 
-        val btnAgregarPlan = findViewById<Button>(R.id.btn_agregar_plan)
+        val btnAgregarPlan = findViewById<ImageButton>(R.id.bt_agregar_plan)
         btnAgregarPlan.setOnClickListener {
             val intent = Intent(this, crear_plan::class.java)
             startActivity(intent)
