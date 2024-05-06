c701a1921799d4020cc5dab9fa4acf3f638ccc1a
JHONATANGold <jhonatan.munozfmm@gmail.com>
2024-05-05 20:25:20 -0500

    Commit 7

--- app/src/main/java/com/example/tadeonow/planes.kt
+++ app/src/main/java/com/example/tadeonow/planes.kt
@@ -6,7 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+data class Plan(
+    val name: String,
+    val imageResourceId: Int,
+    val schedule: String,
+    val content: String,
+    val location: String,
+    val attendees: Int,
+    val comments: String
+)
 class planes : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
